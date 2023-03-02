b41c6bf894f29de55d3fbea6486f9691f32534ea
Shrinivas Petale <64618047+srpetale@users.noreply.github.com>
2023-03-02 09:37:39 -0500

    load is broken into arrival rate and mean holding time

--- cpp_version/traffic_generator.cpp
+++ cpp_version/traffic_generator.cpp
@@ -25,16 +25,14 @@ using namespace std;
 
 // Global variables
 int number_of_nodes = 14;
-vector<float> list_of_datarates = { 40, 100, 400 };
+vector<float> list_of_datarates_in_gbps = { 40, 100, 400 };
 int number_of_connections = 10;
-int load_in_erlang = 600;
-int mean_holding_time = 1;
 
 // Stucts and Class
 struct ConnectionRequest {
     int source = 0;
     int destination = 0;
-    int datarate = 0;
+    int datarate_in_gbps = 0;
     double arrival_time = 0.0;
     double holding_time = 0.0;
 };
@@ -42,13 +40,13 @@ struct ConnectionRequest {
 class NetworkTrafficGenerator {
     /*
     This class generates the network traffic using Poisson distribution. In each iteration,
-    it generates arrival time and holding time. The load (in erlangs) is used as a variable
-    \lambda to generate these values using exponential function. 
+    it generates arrival time, holding time and departure time. The load (in erlangs) is used
+	as a variable \lambda to generate these values using exponential function. 
     */
    private:
    int number_of_nodes;
    int lambda_in_poisson_distribution;
-   int mean_holding_time;
+   double mean_holding_time;
    double current_time;
 
    ConnectionRequest get_connection_data();
@@ -56,23 +54,23 @@ class NetworkTrafficGenerator {
    double random_exponential(double denominator);
 
    public:
-   NetworkTrafficGenerator(int number_of_nodes, int lambda_in_poisson_distribution, int mean_holding_time){
+   NetworkTrafficGenerator(int number_of_nodes, int arrival_rate, int mean_holding_time){
     this->number_of_nodes = number_of_nodes;
-    this->lambda_in_poisson_distribution = lambda_in_poisson_distribution;
+    this->lambda_in_poisson_distribution = arrival_rate;
     this->mean_holding_time = mean_holding_time;
     current_time = 0.0;
    }
    ConnectionRequest get_connection();
 };
 
-ConnectionRequest NetworkTrafficGenerator::get_connection_data(){
+ConnectionRequest NetworkTrafficGenerator::get_connection_data() {
     ConnectionRequest connection_request;
     connection_request.source = rand() % number_of_nodes;
 	do {
 		connection_request.destination = rand() % number_of_nodes;
 	} while (connection_request.destination == connection_request.source);
 
-    connection_request.datarate = list_of_datarates[rand() % list_of_datarates.size()];
+    connection_request.datarate_in_gbps = list_of_datarates_in_gbps[rand() % list_of_datarates_in_gbps.size()];
     
     return connection_request;
 }
@@ -102,16 +100,17 @@ ConnectionRequest NetworkTrafficGenerator::get_connection() {
 }
 
 // Main execution
-int main(){
-    int load_in_erlang = 600;
+int main() {
+	int arrival_rate = 600;
+	double mean_holding_time = 1;
     srand(time(NULL));
-    NetworkTrafficGenerator network_traffic_generator(number_of_nodes, load_in_erlang, mean_holding_time);
+    NetworkTrafficGenerator network_traffic_generator(number_of_nodes, arrival_rate, mean_holding_time);
     
 	int connection_id = 0;
 	while (connection_id < number_of_connections) {
         ConnectionRequest connection_request = network_traffic_generator.get_connection();
 
-        cout << "The connection of datarate " << connection_request.datarate << " Gbps arrives at node " << connection_request.source << " to node " << connection_request.destination << " at " << connection_request.arrival_time << " and exists for " << connection_request.holding_time << ". It exists from " << connection_request.arrival_time << " to " << connection_request.arrival_time+connection_request.holding_time << "." << endl;
+        cout << "The connection of datarate " << connection_request.datarate_in_gbps << " Gbps arrives at node " << connection_request.source << " to node " << connection_request.destination << " at " << connection_request.arrival_time << " and exists for " << connection_request.holding_time << ". It exists from " << connection_request.arrival_time << " to " << connection_request.arrival_time+connection_request.holding_time << "." << endl;
         
         connection_id += 1;
     }
